0aa4a22d9d9ed2bdfdf2af6b48ef33d641b117ee
Andei Aldea <andrei.sateanu@gmail.com>
2018-05-02 15:49:21 +0300

    Add project files.

---
+++ IdleClicker/IdleClicker.Desktop/Program.cs
@@ -0,0 +1,13 @@
+﻿using Urho;
+
+namespace IdleClicker.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyGame(new ApplicationOptions("MyData")).Run();
+            // For a console app Urho will create a Windows/macOS/Linux window using SDL 
+        }
+    }
+}
---
+++ IdleClicker/IdleClicker.Droid/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Org.Libsdl.App;
+using Urho;
+using Urho.Droid;
+
+namespace IdleClicker.Droid
+{
+    [Activity(Label = "IdleClicker.Droid", MainLauncher = true,
+        Icon = "@drawable/icon", Theme = "@android:style/Theme.NoTitleBar.Fullscreen",
+        ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Landscape)]
+    public class MainActivity : Activity
+    {
+        MyGame myGame;
+        UrhoSurfacePlaceholder surface;
+
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var layout = new FrameLayout(this);
+            surface = UrhoSurface.CreateSurface(this);
+            layout.AddView(surface);
+            SetContentView(layout);
+            myGame = await surface.Show<MyGame>(new ApplicationOptions("MyData"));
+            //to stop the game use await surface.Stop().
+        }
+
+        protected override void OnResume()
+        {
+            UrhoSurface.OnResume();
+            base.OnResume();
+        }
+
+        protected override void OnPause()
+        {
+            UrhoSurface.OnPause();
+            base.OnPause();
+        }
+
+        public override void OnLowMemory()
+        {
+            UrhoSurface.OnLowMemory();
+            base.OnLowMemory();
+        }
+
+        protected override void OnDestroy()
+        {
+            UrhoSurface.OnDestroy();
+            base.OnDestroy();
+        }
+
+        public override bool DispatchKeyEvent(KeyEvent e)
+        {
+            if (e.KeyCode == Android.Views.Keycode.Back)
+            {
+                this.Finish();
+                return false;
+            }
+
+            return base.DispatchKeyEvent(e);
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            UrhoSurface.OnWindowFocusChanged(hasFocus);
+            base.OnWindowFocusChanged(hasFocus);
+        }
+    }
+}
+
