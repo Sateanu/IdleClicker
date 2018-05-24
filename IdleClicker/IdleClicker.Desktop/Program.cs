7fc8976895d81d7862c12e4e948d06d50a293570
Radu Coriu <radu.gabrielc@gmail.com>
2018-05-24 19:02:22 +0300

    assets

--- IdleClicker/IdleClicker.Desktop/Program.cs
+++ IdleClicker/IdleClicker.Desktop/Program.cs
@@ -6,7 +6,7 @@ namespace IdleClicker.Desktop
     {
         static void Main(string[] args)
         {
-            new MyGame(new ApplicationOptions("Data")).Run();
+            new MyGame(new ApplicationOptions("Assets/Data")).Run();
             // For a console app Urho will create a Windows/macOS/Linux window using SDL 
         }
     }
